c6822c354208622a7351d7ffdc7f4eb5e273e003
dariuszkubica <10threex01@gmail.com>
2023-01-27 10:40:23 +0100

    Implement ResponsiveLayout

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_example/core/responsive_layout.dart';
 
 import 'widgets/extra_panel.dart';
 import 'widgets/main_content.dart';
@@ -6,6 +7,62 @@ import 'widgets/navigation_panel.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const ResponsiveLayout(
+      ultrawide: _UltraWideHomePage(),
+      wide: _WideHomePage(),
+      narrow: _NarrowWideHomePage(),
+    );
+  }
+}
+
+class _UltraWideHomePage extends StatelessWidget {
+  const _UltraWideHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(
+            width: 300,
+            child: NavigationPanel(),
+          ),
+          Expanded(
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: SizedBox(
+                width: 900,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Expanded(
+                      child: MainContent(),
+                    ),
+                    Expanded(
+                      child: ExtraPanel(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _WideHomePage extends StatelessWidget {
+  const _WideHomePage({
     Key? key,
   }) : super(key: key);
 
@@ -26,6 +83,26 @@ class HomePage extends StatelessWidget {
           Expanded(
             child: ExtraPanel(),
           ),
+        ],
+      ),
+    );
+  }
+}
+
+class _NarrowWideHomePage extends StatelessWidget {
+  const _NarrowWideHomePage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      drawer: const Drawer(child: NavigationPanel()),
+      body: ListView(
+        children: const [
+          MainContent(),
+          ExtraPanel(),
         ],
       ),
     );
